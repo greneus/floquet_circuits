e7449d794ba5813034635ae033442599eecd3d68
Marcell Kovacs <ma7812@me.com>
2023-06-08 17:02:31 +0100

    Adding Pauli evolutions, fixing bugs

--- Random_Cliffords.py
+++ Random_Cliffords.py
@@ -13,87 +13,130 @@ class Random_Clifford_Tableau:
     Class for storing and evolving Clifford tableau for a qubit-chain
     via Aaronson-Gottesman algorithm
     '''
-    
-    def __init__(L = 10, D = 0, nT = 10):
+    def __init__(self, L = 10, D = 0, nT = 10):
         '''
         L: length of qubit chain
         D: number of single-qubit defect chains
         nT: number of Floquet periods
         '''
-        assert L >= 0 and L < 1000
-        assert D >= 0 and D <= 10
-        assert nT >= 1 and nT < 10
+        assert (L > 0 and L < 1000)
+        assert (D >= 0 and D <= 10)
+        assert (nT >= 1 and nT <= 10)
 
         self.L = L
         self.D = D
         self.nT = nT
+        
+        # Integer stabiliser tableau for Pauli's and phases
+        self.tableau = np.zeros((1, 2*self.L + 1)).astype(int)# Initial tableau with only identities
+        # Weights of stabilisers of a superp
+        self.weights = np.ones((1))
+        self.tableau[:, -1] = np.ones(len(self.tableau[:, 0])).astype(int) # Set initial Pauli phase to 1
 
-        self.tableau = np.zeros(2*L + 2) # Initial tableau with only identities
-        self.tableau[-2] = 1 # Set initial Pauli phase to 1
-        self.tableau[-1] = 1 # Set initial Pauli weight to 1
+        self.z_indices = list(np.arange(0, self.L))
+        self.x_indices = list(np.arange(self.L, 2*self.L))
 
-        self.z_indices = list(np.arange(0, L))
-        self.x_indices = list(np.arange(L, 2*L))
-
-        return 
-
-    def Hadamard(i):
+    def Hadamard(self, i):
         '''
         Evolve tableau with Hadamard on qubit i
         '''
         
         # Swap Xi coloumn with Zi coloumn
 
-        self.tableau[:, [i, L+i]] = self.tableau[:, [L+i, i]]
+        # [0, L-1]: z_indices
+        # [L, 2L-1]: x_indices
+        
+        # Swap the ith z-coloumn with the ith x coloumnt
+        self.tableau[:, [i, self.L+i]] = self.tableau[:, [self.L+i, i]]
         
         # Update phase of each coloumn
-        self.tableau[:, 2L] ^= self.tableau[:, i]*self.tableau[:, L+i]
+        self.tableau[:, 2*self.L] = self.tableau[:, 2*self.L]^(self.tableau[:, self.L+i]*self.tableau[:, i])
     
-    def Phase(i):
+    def Phase(self, i):
         '''
         Evolve tableau with Phase gate on qubit i
         '''
         # [0, L-1]: z_indices
         # [L, 2L-1]: x_indices
-        self.tableau[:, i] = np.bitwise_xor(self.tableau[:, i], self.tableau[:, L+i])
+        # Perform XOR on the ith Z coloumn and the ith X coloumn
+        self.tableau[:, i] = self.tableau[:, i]^self.tableau[:, self.L+i]
 
-         # Update phase of each coloumn
-        self.tableau[:, 2L] ^= self.tableau[:, i]*self.tableau[:, L+i]
-
-    def CNOT(i, j):
+        # Update phase of each coloumn
+        self.tableau[:, 2*self.L] = self.tableau[:, 2*self.L]^(self.tableau[:, self.L+i]*self.tableau[:, i])
+        
+        
+    def CNOT(self, i, j):
         '''
         Evolve tableau with CNOT with qubit i as control
         and qubit j as target
         '''
 
-        self.tableau[:, i] = np.bitwise_xor(self.tableau[:, i], self.tableau[:, j])
-        self.tableau[:, L+j] = np.bitwise_xor(self.tableau[:, L+j], self.tableau[:, L+i])
-        self.tableau[:, 2L] ^= self.tableau[:, i]*self.tableau[:, L+j]\
-                               *np.bitwise_xor(\
-                                    np.bitwise_xor(self.tableau[:, L+j], self.tableau[:, i]), \
-                                    np.ones(len(self.tableau[:, i])))
-        
+        self.tableau[:, i] = self.tableau[:, i]^self.tableau[:, j]
+        self.tableau[:, self.L+j] = self.tableau[:, self.L+j]^self.tableau[:, self.L+i]
+        # Update phase of each coloumn
+        self.tableau[:, 2*self.L] = self.tableau[:, 2*self.L]^(self.tableau[:, self.L+i]*self.tableau[:, j])*\
+                                    (self.tableau[:, self.L+j]^self.tableau[:, i]^np.ones(len(self.tableau[:, i])).astype(int))
     
-    def apply_random_clifford(i, j):
+    def X(self, i):
+        '''
+        Apply X-evolution on qubit i
+        '''
+        self.tableau[:, 2*self.L] = self.tableau[:, 2*self.L]^self.tableau[:, i]
+    def Y(self, i):
+        '''
+        Apply iY-evolution on qubit i
+        '''
+        self.tableau[:, 2*self.L] = self.tableau[:, 2*self.L]^self.tableau[:, self.L+i]^self.tableau[:, i]
+    def Z(self, i):
+        '''
+        Apply Z-evolution on qubit i
+        '''
+        self.tableau[:, 2*self.L] = self.tableau[:, 2*self.L]^self.tableau[:, self.L+i]
+    def PH(self, i):
+        '''
+        Apply Phase+Hadamard on qubit i
+        '''
+        temp = np.copy(self.tableau[:, i])
+        self.tableau[:, i] = self.tableau[:, i]^self.tableau[:, self.L+i]
+        self.tableau[:, self.L+i] = temp
+
+    def HP(self, i):
+        '''
+        Apply Hadamard+Phase on qubit i
+        '''
+
+        temp = np.copy(self.tableau[:, self.L+i])
+        self.tableau[:, self.L+i] = self.tableau[:, self.L+i]^self.tableau[:, self.i]
+        self.tableau[:, i] = temp
+
+        self.tableau[:, 2*self.L] = self.tableau[:, 2*self.L]^self.tableau[:, self.L+i]
+
+    def apply_random_clifford(self, i, j):
         '''
         Apply randomly drawn 2-qubit Clifford gate
         between sites i, j
         '''
 
         # Apply C1C2 random single-qubit Cliffords
+        group1 = ["H", "I"]
+        group2 = ["HP", "PH", "I"]
+        group3 = ["X", "Y", "Z", "I"]
+
+        rand = np.randint(0, 3)
 
 
-        return
+
+        return 0
     
-    def clifford_layer():
+    def clifford_layer(self):
         '''
         Apply a layer of pairwise, random Clifford gates 
         '''
-    def defect_layer():
+    def defect_layer(self):
         '''
         Apply a layer of randomly chosen defect gates
         '''
-    def progress_time():
+    def progress_time(self):
         '''
         Apply full-period of Floquet-evolution
         '''
--- test_script.py
+++ test_script.py
@@ -3,94 +3,16 @@ import scipy as sp
 import matplotlib.pyplot as plt
 import random
 
-class Random_Clifford_Tableau: 
-    '''
-    Class for storing and evolving Clifford tableau for a qubit-chain
-    via Aaronson-Gottesman algorithm
-    '''
-    
-    def __init__(L = 10, D = 0, nT = 10):
-        '''
-        L: length of qubit chain
-        D: number of single-qubit defect chains
-        nT: number of Floquet periods
-        '''
-        assert L >= 0 and L < 1000
-        assert D >= 0 and D <= 10
-        assert nT >= 1 and nT < 10
+from Random_Cliffords import Random_Clifford_Tableau
 
-        self.L = L
-        self.D = D
-        self.nT = nT
+tab = Random_Clifford_Tableau(L=5)
+print(tab.tableau)
 
-        self.tableau = np.zeros(2*L + 2) # Initial tableau with only identities
-        self.tableau[-2] = 1 # Set initial Pauli phase to 1
-        self.tableau[-1] = 1 # Set initial Pauli weight to 1
-
-        self.z_indices = list(np.arange(0, L))
-        self.x_indices = list(np.arange(L, 2*L))
-
-        return 
-
-    def Hadamard(i):
-        '''
-        Evolve tableau with Hadamard on qubit i
-        '''
-        
-        # Swap Xi coloumn with Zi coloumn
-
-        self.tableau[:, [i, L+i]] = self.tableau[:, [L+i, i]]
-        
-        # Update phase of each coloumn
-        self.tableau[:, 2L] ^= self.tableau[:, i]*self.tableau[:, L+i]
-    
-    def Phase(i):
-        '''
-        Evolve tableau with Phase gate on qubit i
-        '''
-        # [0, L-1]: z_indices
-        # [L, 2L-1]: x_indices
-        self.tableau[:, i] = np.bitwise_xor(self.tableau[:, i], self.tableau[:, L+i])
-
-         # Update phase of each coloumn
-        self.tableau[:, 2L] ^= self.tableau[:, i]*self.tableau[:, L+i]
-
-    def CNOT(i, j):
-        '''
-        Evolve tableau with CNOT with qubit i as control
-        and qubit j as target
-        '''
-
-        self.tableau[:, i] = np.bitwise_xor(self.tableau[:, i], self.tableau[:, j])
-        self.tableau[:, L+j] = np.bitwise_xor(self.tableau[:, L+j], self.tableau[:, L+i])
-        self.tableau[:, 2L] ^= self.tableau[:, i]*self.tableau[:, L+j]\
-                               *np.bitwise_xor(\
-                                    np.bitwise_xor(self.tableau[:, L+j], self.tableau[:, i]), \
-                                    np.ones(len(self.tableau[:, i])))
-        
-    
-    def apply_random_clifford(i, j):
-        '''
-        Apply randomly drawn 2-qubit Clifford gate
-        between sites i, j
-        '''
-
-        # Apply C1C2 random single-qubit Cliffords
-
-
-        return
-    
-    def clifford_layer():
-        '''
-        Apply a layer of pairwise, random Clifford gates 
-        '''
-    def defect_layer():
-        '''
-        Apply a layer of randomly chosen defect gates
-        '''
-    def progress_time():
-        '''
-        Apply full-period of Floquet-evolution
-        '''
-
-    
+tab.tableau[0, 0] = 1
+print(tab.tableau)
+tab.Hadamard(0)
+print(tab.tableau)
+tab.Phase(0)
+print(tab.tableau)
+tab.CNOT(0, 1)
+print(tab.tableau)
